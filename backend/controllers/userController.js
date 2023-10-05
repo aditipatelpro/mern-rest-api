9226f331af19869e17bb1f46fa8365937883078f
aditipatelpro <aditipatelpro@gmail.com>
2023-10-05 15:43:46 -0500

    feat: added Auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -58,11 +58,15 @@ const loginUser = asyncHandler (async (req, res) => {
     }
 })
 
-// @desc Get user data  
-// @route GET /api/users/me
-// @access Public
+
 const getUser = asyncHandler (async (req, res) => {
-    res.json({ message: 'User data display' })
+    const {_id, name, email} = await User.findById(req.user.id)
+
+    res.status(200).json({ 
+        id: _id,
+        name,
+        email,
+    })
 })
 
 const generateToken = (id) => {
