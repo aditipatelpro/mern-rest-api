44f36be3ff6c18f47b04fdaa425d2db5daeb34ee
aditipatelpro <aditipatelpro@gmail.com>
2023-10-07 17:30:11 -0500

    feat: created goal reducer

---
+++ frontend/src/features/goals/goalService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8000/api/goals/'
+
+const createGoal = async (goalData, token) => {
+    const config = {
+        heaaders: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.post(API_URL, goalData, config)
+    return response.data
+}
+
+const goalService = { createGoal}
+export default goalService
